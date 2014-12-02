692299c91a9828827ec366b7b1f2df7b55d84747
Aurélien Georget <aurelsicoko@gmail.com>
2014-12-02 17:06:21 +0100

    [events] Category requires unique title and icon

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ActiveRecord::Base
+  validates :icon, presence: true
+  validates :title, presence: true, uniqueness: true
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe Category, :type => :model do
     expect(found.title).to eq("Basket-ball")
     expect(found.icon).to eq("http://www.google.png")
   end
+
+  it "requires a title and an icon" do
+    category = Category.new
+    expect(category.valid?).to eq(false)
+
+    category.title = "Rigby"
+    expect(category.valid?).to eq(false)
+
+    category.icon = "http://www.google.png"
+    expect(category.valid?).to eq(true)
+  end
 end
