d3aa1b02001dcd05ec313a4eca57aed9031ee2a5
Aurélien Georget <aurelsicoko@gmail.com>
2014-12-02 17:10:46 +0100

    [events] Add test for event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,5 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Event, :type => :model do
   it "will do something" do
+    event = Event.create(title: "Basket-ball event", cover:"http://www.google.png", likes:"0", description:"This is a short description", adresse:"Tour Eiffel, Paris, 75000")
+    event.save!
+
+    found = Event.last
+    expect(found.title).to eq("Basket-ball event")
+    expect(found.cover).to eq("http://www.google.png")
+    expect(found.likes).to eq(0)
+    expect(found.description).to eq("This is a short description")
+    expect(found.adresse).to eq("Tour Eiffel, Paris, 75000")
   end
 end
