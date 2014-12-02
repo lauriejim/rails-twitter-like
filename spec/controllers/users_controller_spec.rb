32e3f946a81b026cbac87fdb48710d644ef95a3a
Jim LAURIE <hack@jimlaurie.me>
2014-12-02 22:20:34 +0100

    [UsersTests] controller: index swho create

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,45 +3,59 @@ require 'rails_helper'
 RSpec.describe UsersController, :type => :controller do
 
   describe "GET index" do
-    it "returns http success" do
+    it "display user list" do
+      user = User.create(firstname: "Jean", lastname: "Bon", email: "jean@bon.by", rank: "user")
+      user_1 = User.create(firstname: "Jean1", lastname: "Bon", email: "jean1@bon.by", rank: "user")
+      user_2 = User.create(firstname: "Jean2", lastname: "Bon", email: "jean2@bon.by", rank: "user")
+
       get :index
       expect(response).to have_http_status(:success)
+
+      expect(response.body).to include("#{user.firstname}")
+      expect(response.body).to include("#{user_1.firstname}")
+      expect(response.body).to include("#{user_2.firstname}")
     end
   end
 
   describe "GET show" do
-    it "returns http success" do
-      get :show
+    it "display user" do
+      user = User.create(firstname: "Jean", lastname: "Bon", email: "jean@bon.by", rank: "user")
+
+      get :show, id: user.id
       expect(response).to have_http_status(:success)
+
+      expect(response.body).to include("Jean")
+      expect(response.body).to include("Bon")
+      expect(response.body).to include("jean@bon.by")
     end
   end
 
-  describe "GET create" do
-    it "returns http success" do
-      get :create
+  describe "POST create" do
+    it "create new user" do
+
+      post :create, post: {firstname: "Jean", lastname: "Bon", email: "jean@bon.by", rank: "user"}
       expect(response).to have_http_status(:success)
+
+      user = User.last
+      expect(user.firstname).to eq("Jean")
+      expect(user.lastname).to eq("Bon")
+      expect(user.email).to eq("jean@bon.by")
+      expect(user.rank).to eq("user")
     end
   end
 
-  describe "GET new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET update" do
+  #   it "returns http success" do
+  #     put :update
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET destroy" do
+  #   it "returns http success" do
+  #     delete :destroy
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
