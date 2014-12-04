12ff5151f9b27570805624dccd6c1e1990363ae2
Aurélien Georget <aurelsicoko@gmail.com>
2014-12-04 15:27:25 +0100

    [events] Add create method for sport

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -4,4 +4,9 @@ class SportsController < ApplicationController
     @sports = Sport.all
   end
   
+  def create
+    Sport.create(title: params[:post][:title], icon: params[:post][:icon])
+    redirect_to posts_path
+  end
+
 end
