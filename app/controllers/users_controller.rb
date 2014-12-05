28611f0d4e807cda14fc3ddf6068b8523baaae4d
Jim LAURIE <hack@jimlaurie.me>
2014-12-05 15:29:55 +0100

    [UsersTest] handle user creation error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
+  # def index
+  #   @users = User.all
+  # end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  # def show
+  #   @user = User.find(params[:id])
+  # end
 
   def new
     @user = User.new
@@ -13,33 +13,36 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(user_params);
-    user.save!
-
-    redirect_to "/users/#{user.id}"
-  end
-
-  def edit
-    @ranks = RailsTwitterLike::Application::RANKS
-    @user = User.find(params[:id])
-  end
-
-  def update
-    user = User.find(params[:id])
-    if params[:user][:rank]
-      user.update(user_params.merge({rank: params[:user][:rank]}))
+    @user = User.new(user_params);
+    if @user.save
+      redirect_to users_path
     else
-      user.update(user_params)
+      @ranks = RailsTwitterLike::Application::RANKS
+      render :new
     end
-
-    redirect_to "/users/#{user.id}"
   end
 
-  def destroy
-  end
+  # def edit
+  #   @ranks = RailsTwitterLike::Application::RANKS
+  #   @user = User.find(params[:id])
+  # end
+
+  # def update
+  #   user = User.find(params[:id])
+  #   if params[:user][:rank]
+  #     user.update(user_params.merge({rank: params[:user][:rank]}))
+  #   else
+  #     user.update(user_params)
+  #   end
+
+  #   redirect_to "/users/#{user.id}"
+  # end
+
+  # def destroy
+  # end
 
   private
   def user_params
-   params.require(:user).permit(:firstname, :lastname, :email)
+   params.require(:user).permit(:firstname, :lastname, :email, :password, :rank)
   end
 end
