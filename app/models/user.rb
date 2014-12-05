ed39350e906065683c9f4e6ec09cacd72897a28d
Jim LAURIE <hack@jimlaurie.me>
2014-12-05 10:17:11 +0100

    [Users] make link to call controller action sidebar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :firstname, :lastname, :email, :rank, presence: true
+  validates :firstname, :lastname, :email, :rank, :password, presence: true
   validates :email, uniqueness: true
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
