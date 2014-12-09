0a7bef22e8da05e9038e87b6bb007959be7fb758
Jim LAURIE <hack@jimlaurie.me>
2014-12-09 11:59:54 +0100

    [Users] Delete tests folders helper & views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 require 'digest/md5'
 
 class User < ActiveRecord::Base
-  validates :firstname, :lastname, :email, :password, presence: true
+  validates :firstname, :lastname, :email, presence: true
+  validates :password, presence: true, :on => :create
   validates :email, uniqueness: true
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   before_save :hash
