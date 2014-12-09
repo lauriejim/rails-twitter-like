19028bdc3c69674026af13b57e9b67e277b6de54
Aurélien Georget <aurelsicoko@gmail.com>
2014-12-09 12:31:28 +0100

    [users] Fix sport find for exception

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -8,7 +8,7 @@ class Sport < ActiveRecord::Base
   end
 
   def self.find_one(sport_id)
-    Sport.find_by_id(sport_id)
+    Sport.find(sport_id)
   end
 
   def self.count
@@ -16,6 +16,6 @@ class Sport < ActiveRecord::Base
   end
 
   def self.delete_one(sport_id)
-    Sport.destroy(sport_id)
+    Sport.destroy(event_id)
   end
 end
