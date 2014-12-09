d63235261248dd7bbfb4508bf88c04e70c33c22a
Jérémy <jeremy@Air-de-Jeremy.home>
2014-12-10 00:45:03 +0100

    Beginning inte

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,9 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( admin.css )
 
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
