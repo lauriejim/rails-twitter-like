af39f14994a0cd725dbc6deee93dc99a66dff47f
Jim LAURIE <hack@jimlaurie.me>
2014-12-09 10:29:59 +0100

    [Users] try validate password blank on update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,17 @@ require 'digest/md5'
 
 class User < ActiveRecord::Base
   validates :firstname, :lastname, :email, :password, presence: true
+  validates :password, :allow_blank => true, :on => :update
   validates :email, uniqueness: true
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  before_create :hash
-  before_update :hash
+  before_save :hash
 
   private
   def hash
-    self.password = Digest::MD5.hexdigest(self.password)
+    unless self.password
+      self.password = Digest::MD5.hexdigest(self.password)
+    else
+      self.password = nil
+    end
   end
 end
