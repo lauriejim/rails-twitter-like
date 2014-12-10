9d178f3e2a4fb4e49173e037ddd52b7e11766774
Jim LAURIE <hack@jimlaurie.me>
2014-12-10 16:42:24 +0100

    [Follow] link users and sports

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141204130111) do
+ActiveRecord::Schema.define(version: 20141210154059) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20141204130111) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "sport_id"
+  end
+
+  create_table "follows", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "sport_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "sports", force: true do |t|
