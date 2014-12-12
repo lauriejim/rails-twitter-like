2de2768aa253ca179b08e12807bc5eff03ac0e02
Jérémy <jeremy@MacBook-Air-de-Jeremy.local>
2014-12-12 01:15:38 +0100

    Filter design, selections sports dynamicand single event dynamic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,4 +6,25 @@ $(document).ready(function() {
   $('.event-border').each(function(){
     $(this).css('borderTop', '4px solid '+$(this).data('color'));
   });
+
+   $('.sport-border').each(function(){
+    $(this).css('borderTop', '4px solid '+$(this).data('color'));
+  });
+
+   $(".sport-selection").mouseover(function () {
+      $(this).find('img').css('opacity', '0.7');
+    }).mouseout(function() {
+      $(this).find('img').css('opacity', '0');
+    });
+
+    $('.filter-sport').on('click', function(){
+      if($(this).hasClass('active')){
+        $(this).removeClass('active');
+      }
+      else{
+        $(this).addClass('active');
+      }
+    });
+
+    $('#page-event').css('background-image', 'url(/uploads/'+$('#event').data('sport')+')');
 });
